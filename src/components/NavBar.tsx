8fc95b373a8a9222452552e9b7f8c371744c0248
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-10-23 19:27:56 +0100

    Updated content of NavBar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,9 +1,51 @@
 import React from 'react';
 import '../styles/navbar.scss';
+import {
+    FaInstagram,
+    FaLinkedin,
+    FaFacebook,
+    FaTwitter,
+    FaYoutube,
+  } from "react-icons/fa";
 
 const NavBar = () => {
   return (
-    <div className='NavBar'>NavBar</div>
+    <div className='NavBar'>
+        {/* {SideBar} */}
+        <div className='wrapper'>
+            <span>Luna Smith</span>
+            <div className="social">
+                <a  href="//www.instagram.com/lunasmithart/"
+                    target="_blank"
+                    rel="noreferrer" 
+                    aria-label="Instagram page">
+                     <FaInstagram />
+                </a>
+                <a href="//www.facebook.com/artlunasmith"
+                    target="_blank"
+                    rel="noreferrer" 
+                    aria-label="Facebook page">
+                    <FaFacebook />
+                </a>
+                <a  href="//twitter.com/LunaSmithArt"
+                    target="_blank"
+                    aria-label="Twitter page"
+                    rel="noreferrer">
+                    <FaTwitter />
+                </a>
+                <a  href="//www.linkedin.com/in/luna-smith/"
+                    rel="noreferrer" 
+                    aria-label="YouTube page">
+                    <FaYoutube />
+                </a>
+                <a  href="//www.linkedin.com/in/luna-smith/"
+                    rel="noreferrer" 
+                    aria-label="LinkendIn page">
+                    <FaLinkedin />
+                </a>
+            </div>
+        </div>
+    </div>
   )
 }
 
