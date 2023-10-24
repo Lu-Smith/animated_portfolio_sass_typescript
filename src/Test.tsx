b88d4d59bf9a6a6d935f3ceabad11b0b9a1712f5
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-10-24 17:32:41 +0100

    Updated app.scss

--- src/Test.tsx
+++ src/Test.tsx
@@ -3,10 +3,11 @@ import { motion } from 'framer-motion';
 const Test = () => {
 
     const variants = {
-        visible: { 
+        visible: (index: number) => ({ 
             opacity: 1, 
             x: 100, 
-            transition: { staggerChildren: 0.3}},
+            transition: { delay: index * 0.3},
+        }),
         hidden: { opacity: 0},
     }
 
@@ -22,7 +23,7 @@ const Test = () => {
             <motion.ul
             initial='hidden' animate='visible' variants={variants}>
                 {items.map((item: string, index: number) => {
-                    return <motion.li key={index} variants={variants}>
+                    return <motion.li key={index} variants={variants} custom={index}>
                         {item}
                     </motion.li>
                 })}
