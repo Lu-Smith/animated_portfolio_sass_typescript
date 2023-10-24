f57b9c0e1e3751b528d81dd1860e9b63b228066f
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-10-24 17:33:30 +0100

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,20 @@
 import React from 'react';
 import './styles/app.scss';
-// import NavBar from './components/NavBar';
-import Test from './Test';
+import NavBar from './components/NavBar';
+
 
 function App() {
   return (
     <div className="App">
-      {/* <section>
+      <section>
         <NavBar />
       </section>
       <section>Parallax</section>
       <section>Art Projects</section>
       <section>Parallax</section>
-      <section>Contact</section> */}
-      <Test />
-      <Test />
+      <section>Contact</section>
     </div>
   );
 }
 
-export default App;
+export default App; 
