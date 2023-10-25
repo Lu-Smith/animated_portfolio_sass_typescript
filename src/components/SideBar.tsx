197ed6cd7f56654861466c88d407068fb6d8f9b5
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-10-25 14:38:09 +0100

    Added Links and ToggleButton to SideBar

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from 'react';
+import Links from './Links';
+import ToogleButton from './ToogleButton';
 
 const SideBar = () => {
   return (
     <div className='SideBar'>
         <div className="container">
-            
+            <Links />
         </div>
+        <ToogleButton />
     </div>
   )
 }
