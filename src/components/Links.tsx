b1d2cf94d8733a1e8d41113a55d0d5102057530c
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-10-26 13:37:06 +0100

    Updated Links.tsx content

--- src/components/Links.tsx
+++ src/components/Links.tsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import React from 'react';
+import { NavLink } from 'react-dom';
 
 const Links = () => {
+
+  const items = ['Home', 'Projects', 'Portfolio', 'About', 'Contact']
   return (
-    <div className='Links'>Links</div>
+    <div className='Links'>
+      {items.map((item, index) => {
+        return <NavLink key={index}>{item}</NavLink>
+      })}
+    </div>
   )
 }
 
