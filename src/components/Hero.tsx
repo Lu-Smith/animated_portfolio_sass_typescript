0676c5f82b79ad2eb23d3ebc775360fcc5ee3bde
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-10-26 19:06:27 +0100

    Updated Hero content
    added scroll.png

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,16 +1,22 @@
 import React from 'react';
 import Witch from '../assets/Little Witch.png';
+import Scroll from '../assets/scroll.png';
+
 
 const Hero = () => {
   return (
     <div className='Hero'>
-          <div className="textContainer">
+        <div className="wrapper">
+            
+        </div>
+        <div className="textContainer">
             <h2>Luna Smith</h2>
             <h1>Web developer and Artist</h1>
             <div className='buttons'>
                 <button>See the Latest Works</button>
                 <button>Contact Me</button>
             </div>
+            <img src={Scroll} alt="scroll" />
         </div>
         <div className="imageContainer">
             <img src={Witch} alt="Little Witch" />
