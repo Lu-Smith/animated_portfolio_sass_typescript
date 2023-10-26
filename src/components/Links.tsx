2ebf2274f40e194b914a28a19a91d781ad3e106f
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-10-26 16:17:48 +0100

    Updated Links.tsx animtation

--- src/components/Links.tsx
+++ src/components/Links.tsx
@@ -1,14 +1,48 @@
 import React from 'react';
+import { motion } from 'framer-motion';
+
+const variants = {
+  open: {
+    transition: {
+       staggerChildren: 0.1,
+    },
+  },
+  closed: {
+    transition: {
+      staggerChildren: 0.05,
+      staggerDirection: -1,
+    }
+  }
+}
+
+const itemVariants = {
+  open: {
+    y: 0,
+    opacity: 1,
+  },
+  closed: {
+   y: 50, 
+   opacity: 0,
+  }
+}
 
 const Links = () => {
 
-  const items = ['Home', 'Projects', 'Portfolio', 'About', 'Contact']
+  const items = ['Home', 'Projects', 'Portfolio', 'About', 'Contact'];
+
   return (
-    <div className='Links'>
+    <motion.div className='Links' variants={variants}>
       {items.map((item, index) => {
-        return <a key={index} href={`#${item}`}>{item}</a>
+        return <motion.a 
+        key={index} 
+        href={`#${item}`} 
+        variants={itemVariants}
+         whileHover={{ scale: 1.1 }}
+         whileTap={{ scale: 0.95 }}>
+          {item}
+        </motion.a>
       })}
-    </div>
+    </motion.div>
   )
 }
 
