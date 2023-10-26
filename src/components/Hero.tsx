26304d71b29f814ae99e1085d19555ebc5518713
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-10-26 19:12:43 +0100

    Updated styling in Hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -7,16 +7,15 @@ const Hero = () => {
   return (
     <div className='Hero'>
         <div className="wrapper">
-            
-        </div>
-        <div className="textContainer">
-            <h2>Luna Smith</h2>
-            <h1>Web developer and Artist</h1>
-            <div className='buttons'>
-                <button>See the Latest Works</button>
-                <button>Contact Me</button>
+            <div className="textContainer">
+                <h2>Luna Smith</h2>
+                <h1>Web developer and Artist</h1>
+                <div className='buttons'>
+                    <button>See the Latest Works</button>
+                    <button>Contact Me</button>
+                </div>
+                <img src={Scroll} alt="scroll" />
             </div>
-            <img src={Scroll} alt="scroll" />
         </div>
         <div className="imageContainer">
             <img src={Witch} alt="Little Witch" />
