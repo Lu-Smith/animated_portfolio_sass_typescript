810719ee42acf92651588ade1b2e1944e6a662b8
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-10-26 18:57:55 +0100

    Updated content of Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,7 +4,15 @@ import Witch from '../assets/Little Witch.png';
 const Hero = () => {
   return (
     <div className='Hero'>
-        <div className="container">
+          <div className="textContainer">
+            <h2>Luna Smith</h2>
+            <h1>Web developer and Artist</h1>
+            <div className='buttons'>
+                <button>See the Latest Works</button>
+                <button>Contact Me</button>
+            </div>
+        </div>
+        <div className="imageContainer">
             <img src={Witch} alt="Little Witch" />
         </div>
     </div>
