c472a700d6742229698c8f07ba6d2d580b0beb61
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-10-27 12:34:58 +0100

    Updated Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,21 +1,37 @@
 import React from 'react';
 import Witch from '../assets/Little Witch.png';
 import Scroll from '../assets/scroll.png';
+import { motion } from 'framer-motion';
+
+const textVariants = {
+    initial: {
+        x: -500,
+        opacity: 0,
+    },
+    animate: {
+        x: 0,
+        opacity: 1,
+        transition: {
+            duration: 1,
+            staggerChildren: 0.1,
+        }
+    }
+}
 
 
 const Hero = () => {
   return (
     <div className='Hero'>
         <div className="wrapper">
-            <div className="textContainer">
-                <h2>Luna Smith</h2>
-                <h1>Web developer and Artist</h1>
-                <div className='buttons'>
-                    <button>See the Latest Works</button>
-                    <button>Contact Me</button>
-                </div>
-                <img src={Scroll} alt="scroll" />
-            </div>
+            <motion.div className="textContainer" variants={textVariants}>
+                <motion.h2 variants={textVariants}>Luna Smith</motion.h2>
+                <motion.h1 variants={textVariants}>Web developer and Artist</motion.h1>
+                <motion.div variants={textVariants} className='buttons'>
+                    <motion.button variants={textVariants}>See the Latest Works</motion.button>
+                    <motion.button variants={textVariants}>Contact Me</motion.button>
+                </motion.div>
+                <motion.img variants={textVariants} src={Scroll} alt="scroll" />
+            </motion.div>
         </div>
         <div className="slidingTextContainer">
             Creator of code-based masterpieces and fine art.
