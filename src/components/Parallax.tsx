0622bb5de304279b622a009d76043dfa571b6290
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-10-31 15:46:27 +0000

    Updated Parallax animations

--- src/components/Parallax.tsx
+++ src/components/Parallax.tsx
@@ -1,19 +1,45 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { motion, useScroll, useTransform } from 'framer-motion';
 
 interface ParallaxType {
     type: string,
 }
 
 const Parallax = ({ type }: ParallaxType) => {
+
+  const ref = useRef<HTMLDivElement>(null)
+
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ['start start', 'end start']
+  })
+
+  const yBg = useTransform(
+    scrollYProgress,
+    [0, 1],
+    ['0%', '100%']
+  )
+
+  const yText = useTransform(
+    scrollYProgress,
+    [0, 1],
+    ['0%', '500%']
+  )
+
   return (
     <div 
     className='Parallax' 
+    ref={ref}
     style={{background: type === 'projects' ? 
     '$standardSection-background ': '$parallaxSection-background'}}>
-        <h1>{type === 'projects' ? 'What I Do?' : 'What I Did?'}</h1>
-        <div className="mountains"></div>
-        <div className="planets"></div>
-        <div className="stars"></div>
+        <motion.h1 style={{y: yText}}>{type === 'projects' ? 'What I Do?' : 'What I Did?'}</motion.h1>
+        <motion.div className="mountains"></motion.div>
+        <motion.div 
+        className="planets"
+        style={{y: yBg}}></motion.div>
+        <motion.div 
+        className="stars"
+        style={{x: yBg}}></motion.div>
     </div>
   )
 }
