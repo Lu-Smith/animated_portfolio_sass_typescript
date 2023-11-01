5bf965ef4f6b3f827a3b713e785973fbea6971a3
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-11-01 14:01:15 +0000

    Updated content of Projects.tsx

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import React from 'react';
+import { motion } from 'framer-motion';
 
 const Projects = () => {
   return (
-    <div className='Projects'>Projects</div>
+    <div className='Projects'>
+        <div className="textContainer">
+            <p>
+                I focus on bringing new ideas to life
+                  <br /> and creating art!
+
+            </p>
+        </div>
+        <div className="titleContainer">
+            
+        </div>
+        <div className="listContainer">
+            
+        </div>
+    </div>
   )
 }
 
