bd29ad484736a60275a4edfb3e238a9a7a41642f
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-11-04 14:28:33 +0000

    Added Portfolio to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import NavBar from './components/NavBar';
 import Hero from './components/Hero';
 import Parallax from './components/Parallax';
 import Projects from './components/Projects';
+import Portfolio from './components/Portfolio';
 
 function App() {
   return (
@@ -21,7 +22,9 @@ function App() {
       <section id='Portfolio'>
         <Parallax type='portfolio' />
       </section>
-      <section >Portfolio</section>
+      <section >
+        <Portfolio />
+      </section>
       <section id='About'>Parallax</section>
       <section >About</section>
       <section id='Contact'>Parallax</section>
