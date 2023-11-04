376e15edba48b6eb44329f9c87f898743f2a56f2
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-11-04 16:58:57 +0000

    Updated Portfolio.tsx
    Added projects images

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -1,5 +1,32 @@
 import React from 'react'
 
+const items = [
+    {
+        id: 1,
+        title: 'React',
+        src: '../assets/12R.jpg',
+        alt: 'React projects: Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda culpa, odio similique reiciendis animi debitis at necessitatibus explicabo excepturi voluptate dignissimos ipsa. Eos modi excepturi voluptatum ullam quos ea culpa?',
+    },
+    {
+        id: 2,
+        title: 'JavaScriprt',
+        src: '../assets/16.jpg',
+        alt: 'React projects: Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda culpa, odio similique reiciendis animi debitis at necessitatibus explicabo excepturi voluptate dignissimos ipsa. Eos modi excepturi voluptatum ullam quos ea culpa?',
+    },
+    {
+        id: 3,
+        title: 'Vue',
+        src: '../assets/10R.jpg',
+        alt: 'React projects: Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda culpa, odio similique reiciendis animi debitis at necessitatibus explicabo excepturi voluptate dignissimos ipsa. Eos modi excepturi voluptatum ullam quos ea culpa?',
+    },
+    {
+        id: 4,
+        title: 'Games',
+        src: '../assets/13.jpg',
+        alt: 'React projects: Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda culpa, odio similique reiciendis animi debitis at necessitatibus explicabo excepturi voluptate dignissimos ipsa. Eos modi excepturi voluptatum ullam quos ea culpa?',
+    }
+]
+
 const Portfolio = () => {
   return (
     <div className='Portfolio'>Portfolio</div>
