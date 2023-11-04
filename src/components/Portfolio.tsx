3fd99ef0d17509ef6637d777ada4dd40a8c0acd1
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-11-04 18:15:55 +0000

    Updated animations in Portfolio.tsx

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import { motion, useScroll, useSpring } from 'framer-motion';
 
 interface ItemType {
     title: string;
@@ -43,8 +44,25 @@ const Single = ({ title, src, alt, id }: ItemType) => {
 }
 
 const Portfolio = () => {
+
+    const ref = useRef<HTMLDivElement>(null);
+
+    const { scrollYProgress } = useScroll({
+        target: ref, 
+        offset: ["end end", "start start"],
+    });
+
+    const scaleX = useSpring( scrollYProgress, {
+        stiffness: 100,
+        damping: 30,
+    })
+
   return (
-    <div className='Portfolio'>
+    <div className='Portfolio' ref={ref}>
+        <div className="progress">
+            <h1>Featured Works</h1>
+            <motion.div style={{ scaleX }} className="progressBar"></motion.div>
+        </div>
         {items.map(item => {
             return <Single {...item} key={item.id} />
         })}
