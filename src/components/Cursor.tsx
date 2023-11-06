01dac73fa64d1c987d82203481fc26b8a8ea174a
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-11-06 20:02:51 +0000

    Updated content of Cursor.tsx

--- src/components/Cursor.tsx
+++ src/components/Cursor.tsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Cursor = () => {
-  return (
+
+    const [ position, setPosition ] = useState({x:0, y:0});
+  
+     useEffect(() => {
+        const mouseMove = (e: MouseEvent) => {
+            setPosition({x: e.clientX, y: e.clientY})
+        }
+
+        window.addEventListener("mousemove", mouseMove);
+     
+        return () => {
+            window.removeEventListener("mousemove", mouseMove);
+        }
+    })   
+  
+    return (
     <div className='Cursor'>Cursor</div>
   )
 }
