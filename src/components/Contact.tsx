387befc8c84a7042aac92cb29f9da5f67afdd014
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-11-06 12:18:18 +0000

    Updated content of Contact.tsx

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -2,7 +2,26 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div className='Contact'>Contact</div>
+    <div className='Contact'>
+        <div className="textContainer">
+            <h1>Let's work together</h1>
+            <div className="item">
+                <h2>Mail</h2>
+                <span>lunasmithart@gmail.com</span>
+            </div>
+            <div className="item">
+                <h2>Address</h2>
+                <span>Edinburgh, Scotland 🦄</span>
+            </div>
+            <div className="item">
+                <h2>Phone</h2>
+                <span>07863734874</span>
+            </div>
+        </div>
+        <div className="formContainer">
+
+        </div>
+    </div>
   )
 }
 
